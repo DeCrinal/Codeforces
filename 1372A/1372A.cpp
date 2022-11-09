37a3dd46078f40c63376cbe40f52fd84096fa129
DeCrinal <90postbox@gmail.com>
2022-11-10 00:46:12 +0300

    add 1372A, 31ms 3700KB

---
+++ 1372A/1372A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t,n;
+    cin>>t;
+    for(int i = 0; i<t;i++){
+        cin>>n;
+        for(int j=0;j<n;j++){
+            cout<<1<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+
